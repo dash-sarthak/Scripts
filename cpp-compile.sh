4c9639ebe0076d8595ce1d8bfb68a60ae6c07e69
Sarthak Dash <sarthak@localhost.localdomain>
2021-04-29 06:35:32 +0530

    updated pyproject cpp-compile

--- cpp-compile.sh
+++ cpp-compile.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 name=$( echo $1 | cut -d'.' -f 1 )
-
 g++ -o $name $1
--- pyproject.sh
+++ pyproject.sh
@@ -2,5 +2,8 @@ name=$1
 
 mkdir $name
 cd $name
-virtualenv .
+conda create --name $name python=3.7 pylint
+conda activate $name
+pip install pynvim
 touch main.py
+touch structure.md
