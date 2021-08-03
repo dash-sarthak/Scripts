a4fb7a878fa1f30d4a899b051bf9cc9e14b19175
sarthak <sarthaksash99@outlook.com>
2021-08-03 19:12:21 +0530

    improved notetaker script

---
+++ javaproj.sh
@@ -0,0 +1,4 @@
+mkdir $1 && cd $1
+mkdir src && cd src
+
+touch Main.java
--- note_taker.sh
+++ note_taker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-file_name="$HOME/Desktop/Notes/note-$(date +%d-%m-%Y).md"
+file_name="$HOME/Notes/note-$(date +%d-%m-%Y).md"
 
 if [ ! -f $file_name ]; then
     echo "# Notes for $(date +%Y-%m-%d)" > $file_name
 fi
 
-nvim -c "norm G2o" \
+nvim -c "norm Go" \
     -c "norm Go## $(date +%H:%M)" \
     -c "norm Go" \
     -c "norm zz" \
